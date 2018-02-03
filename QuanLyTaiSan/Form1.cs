bdc9e212049bca4b98774f92d922b0ca2d1d20ca
TanNguyen <ngoctancd11cntt2@gmail.com>
2018-02-03 19:58:18 +0700

    Update

--- QuanLyTaiSan/Form1.cs
+++ QuanLyTaiSan/Form1.cs
@@ -15,5 +15,10 @@ namespace QuanLyTaiSan
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
